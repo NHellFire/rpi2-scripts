04dfcdeae19f47d3cbede726d9bb91162a844685
NHellFire <nathan.renniewaldock@gmail.com>
2015-02-26 14:23:33 +0000

    Kodi build script for compilation on Raspberry Pi

---
+++ kodi/build-native.sh
@@ -0,0 +1,6 @@
+export CFLAGS="-I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads/ -I/opt/vc/include/interface/vmcs_host/linux -fPIC -pipe -mcpu=cortex-a7 -mtune=cortex-a7 -mfpu=vfp -mfloat-abi=hard -mabi=aapcs-linux"
+export CXXFLAGS="$CFLAGS"
+LDFLAGS="-L/opt/vc/lib"
+
+./configure --prefix=/opt/xbmc-bcm --enable-gles --disable-sdl --disable-x11 --disable-xrandr --disable-openmax --disable-optical-drive --disable-dvdcss --disable-joystick --disable-debug --disable-crystalhd --disable-vtbdecoder --disable-vaapi --disable-vdpau --disable-pulse --disable-projectm --with-platform=raspberry-pi --enable-optimizations --enable-libcec --enable-player=omxplayer
+
