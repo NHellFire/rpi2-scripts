c53b9eb9ab8bbd2130b914dde0bbebb8427b45fb
NHellFire <nathan.renniewaldock@gmail.com>
2015-03-31 17:27:46 +0100

    libcec: Run bootstrap if configure script is missing

--- libcec/build-native.sh
+++ libcec/build-native.sh
@@ -1,3 +1,5 @@
 export CFLAGS="-fPIC -pipe -mcpu=cortex-a7 -mtune=cortex-a7 -march=armv7ve -mfpu=neon-vfpv4 -mfloat-abi=hard -mabi=aapcs-linux -O3"
 export CXXFLAGS=$CFLAGS
+
+[ -f configure ] || ./bootstrap
 ./configure --enable-rpi --with-rpi-include-path=/opt/vc/include --with-rpi-lib-path=/opt/vc/lib --enable-exynos=no
