6c9c196b33ea42cc9459c446af088f56899d47b9
NHellFire <nathan.renniewaldock@gmail.com>
2015-03-31 17:28:09 +0100

    libcec: Use gold linker

--- libcec/build-native.sh
+++ libcec/build-native.sh
@@ -1,5 +1,9 @@
 export CFLAGS="-fPIC -pipe -mcpu=cortex-a7 -mtune=cortex-a7 -march=armv7ve -mfpu=neon-vfpv4 -mfloat-abi=hard -mabi=aapcs-linux -O3"
 export CXXFLAGS=$CFLAGS
 
+export LD=/usr/bin/ld.gold
+export LDFLAGS="-Wl,--as-needed"
+
+
 [ -f configure ] || ./bootstrap
 ./configure --enable-rpi --with-rpi-include-path=/opt/vc/include --with-rpi-lib-path=/opt/vc/lib --enable-exynos=no
