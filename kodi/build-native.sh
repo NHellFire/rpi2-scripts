3b93cfe6e313ba3a32d560f597e63013c8f1969c
NHellFire <nathan.renniewaldock@gmail.com>
2015-04-28 21:12:27 +0100

    kodi: Update flags for gold linker
    For some reason gold can't find libs in /usr/local, so specify the path

--- kodi/build-native.sh
+++ kodi/build-native.sh
@@ -1,8 +1,12 @@
-export CFLAGS="-I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads/ -I/opt/vc/include/interface/vmcs_host/linux -fPIC -pipe -mcpu=cortex-a7 -mtune=cortex-a7 -march=armv7ve -mfpu=neon-vfpv4 -mfloat-abi=hard -mabi=aapcs-linux -O3"
+export CFLAGS="-I/opt/vc/include -I/optvc/include/interface/vcos/pthreads/ -I/opt/vc/include/interface/vmcs_host/linux -fPIC -pipe -mcpu=cortex-a7 -mtune=cortex-a7 -march=armv7ve -mfpu=neon-vfpv4 -mfloat-abi=hard -mabi=aapcs-linux -O3"
 export CXXFLAGS="$CFLAGS"
 
-export LD=/usr/bin/ld.gold
 export LDFLAGS="-L/opt/vc/lib -Wl,--as-needed"
+
+# gold linker
+export LDFLAGS="$LDFLAGS -L/usr/local/lib"
+export CFLAGS="$CFLAGS -fuse-ld=gold"
+export CXXFLAGS="$CXXFLAGS -fuse-ld=gold"
 
 export FFMPEG_OPTS="--cpu=armv7ve"
 if [ ! -f configure ] || ! grep -qF 'FFMPEG_OPTS="${FFMPEG_OPTS} -r"' configure.in; then
